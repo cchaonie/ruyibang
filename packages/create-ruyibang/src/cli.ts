81d90656b0af6d587ead6b1c24a39bf71b7d7b61
fightant1w1ll <nc_8093@163.com>
2023-02-05 09:53:36 +0800

    Add prompts questions

--- packages/create-ruyibang/src/cli.ts
+++ packages/create-ruyibang/src/cli.ts
@@ -1,1 +1,24 @@
-console.log('hello create-ruyibang!');
+import prompts from 'prompts';
+import { applicationTypes } from './constants';
+
+const getAppName = async () =>
+  await prompts({
+    type: 'text',
+    name: 'appName',
+    message: 'Please enter the name of your application',
+  });
+
+const selectAppType = async () =>
+  await prompts({
+    type: 'select',
+    name: 'appType',
+    message: 'Please the type of your application',
+    choices: applicationTypes,
+  });
+
+const run = async () => {
+  const { appName } = await getAppName();
+  const { appType } = await selectAppType();
+
+  console.log(appName, appType);
+};
---
+++ packages/create-ruyibang/src/constants.ts
@@ -0,0 +1,18 @@
+export const applicationTypes = [
+  {
+    title: 'taro-mini-app',
+    description: 'A starter mini app created by taro',
+  },
+  {
+    title: 'react-spa',
+    description: 'A starter react SPA',
+  },
+  {
+    title: 'react-ssr',
+    description: 'A starter react ssr app',
+  },
+  {
+    title: 'node-api',
+    description: 'An API server created with node.js',
+  },
+];
